a3f366ad051717bb0d6dfd09f9dac75dec08a135
Nish-2611 <74815765+Nish-2611@users.noreply.github.com>
2020-11-30 13:50:13 +0530

    removed paranthesis

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var btnTranslate= document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#text-input");
 var outputDiv=document.querySelector("#output-text");
 
-function clickhandler() {
+function clickhandler(){
     outputDiv.innerText="hello";
 };
 
-btnTranslate.addEventListener('Click', clickhandler())
+btnTranslate.addEventListener('Click',clickhandler);
