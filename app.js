dc86cf6715d96db92dab692ecf3337f851b48a7b
Nish-2611 <74815765+Nish-2611@users.noreply.github.com>
2020-11-30 20:11:31 +0530

    called server and output translation

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 var btnTranslate=document.querySelector("#btn-tanslate");
-var txtInput=document.querySelector("#text-input");
+var textInput=document.querySelector("#text-input");
 var outputDiv=document.querySelector("#output-text");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function gettranslationURL(inputtext){
+
+    return serverURL +"?" +"text="+inputtext
+}
+
+function errorhandler(error){
+console.log("error occured",error);
+alert("Something Wrong with the server Please try again after some time")
+}
+
 function clickHandler() {
-    outputDiv.innerText = txtInput.value;
+    //outputDiv.innerText = txtInput.value;
+ 
+    var inputtext= textInput.value;
+
+//calling server
+fetch(gettranslationURL(inputtext))
+.then(response => response.json())
+.then(json=>{
+   var translatedtext=json.contents.translated;
+   outputDiv.innerText=translatedtext;
+})
+.catch(errorhandler)
 };
 
 btnTranslate.addEventListener("click",clickHandler);
